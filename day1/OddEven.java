785ee70ca1e34ee590cc2a51fdc4c8afbe90c774
priya-243 <priyaraje243@gmail.com>
2025-07-14 13:16:37 +0530

    day1

---
+++ day1/OddEven.java
@@ -0,0 +1,16 @@
+package week1.day2;
+
+public class OddEven {
+	public static void main(String[] args) {
+		int maxRange;
+		maxRange=10;
+		for(int i=1;i<=maxRange;i++)
+		{
+			if(i%2==1)
+			{
+				System.out.println(i);
+			}
+		}
+	}
+
+}
